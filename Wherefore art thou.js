8999fb1651185d5dbb2b7b3c8ef4a97dc7c49528
Nasim Ahmad Khan <40820072+NakRex@users.noreply.github.com>
2021-01-30 15:20:26 +0530

    Update Wherefore art thou.js

--- Wherefore art thou.js
+++ Wherefore art thou.js
@@ -1,17 +1,24 @@
 
-whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 });
-
 function whatIsInAName(collection, source) {
-  var arr=[];
+  
   var srcKeys = Object.keys(source);
-  collection.filter(function(clobj){
-    for(let i=0; i<srcKeys.length; i++){
-      if(clobj[srcKeys[i]]===source[srcKeys[i]]){
-        arr.push(clobj);
+
+  // filter the collection
+  return collection.filter(function(obj) {
+    for (var i = 0; i < srcKeys.length; i++) {
+      if (
+        !obj.hasOwnProperty(srcKeys[i]) ||
+        obj[srcKeys[i]] !== source[srcKeys[i]]
+      ) {
+        return false;
       }
     }
-   
+    return true;
   });
-console.log(arr);
-return arr;
 }
+
+// test here
+whatIsInAName(
+  [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }
+  ], { last: "Capulet" }
+);
